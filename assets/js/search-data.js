32a655a1106e1bd3314af4f55704bffcd7fa5294
Paleuna <Paleuna@users.noreply.github.com>
2026-07-02 23:25:28 +0000

    Deploying to gh-pages from @ Paleuna/Paleuna.github.io@da09d85bfb1ec37afc24756c14f69f15aaaab66c 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,96 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "news-two-papers-are-accepted-to-cogsci-2026",
+          title: 'Two papers are accepted to CogSci 2026.',
+          description: "",
+          section: "News",},{id: "news-i-ll-attend-the-diverse-intelligences-summit-2026-at-princeton-ai-lab",
+          title: 'I’ll attend the Diverse Intelligences Summit 2026 at Princeton AI lab.',
+          description: "",
+          section: "News",},{id: "news-one-paper-is-accepted-to-icml-mech-interp-2026-as-spotlight-23-800",
+          title: 'One paper is accepted to ICML Mech Interp 2026 as spotlight (23/800).',
+          description: "",
+          section: "News",},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%7A%68%75%34%31@%67%61%74%65%63%68.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/zhimin-hu-a29a13246", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Paleuna", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://x.com/paleunahu", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
